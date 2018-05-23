420252926d821ab37482716b70d75228311c958e
Andrew Piper <piper.andrew@spcollege.edu>
2018-05-23 11:21:05 -0400

    Made sure to only load token metadata once.

--- src/Threax.AspNetCore.JwtCookieAuth/TokenValidationParametersProvider.cs
+++ src/Threax.AspNetCore.JwtCookieAuth/TokenValidationParametersProvider.cs
@@ -12,48 +12,67 @@ using System.Threading.Tasks;
 
 namespace Threax.AspNetCore.JwtCookieAuth
 {
-    class TokenValidationParametersProvider : ITokenValidationParametersProvider
+    class TokenValidationParametersProvider : ITokenValidationParametersProvider, IDisposable
     {
+        private SemaphoreSlim semaphoreSlim = new SemaphoreSlim(1, 1);
+        private TokenValidationParameters tokenValidationParameters;
+
         public TokenValidationParametersProvider()
         {
             
         }
 
+        public void Dispose()
+        {
+            semaphoreSlim.Dispose();
+        }
+
         public async Task<TokenValidationParameters> GetTokenValidationParameters(JwtCookieAuthenticationOptions options)
         {
-            //Grab the metadata
-            using (var handler = new HttpClientHandler())
-            using (var httpClient = new HttpClient(handler))
+            await semaphoreSlim.WaitAsync();
+            try
             {
-                httpClient.Timeout = TimeSpan.FromMinutes(1);
-                httpClient.MaxResponseContentBufferSize = 1024 * 1024 * 10; // 10 MB
-
-                Uri metadataUri = new Uri(new Uri(options.Authority), options.MetadataPath);
-
-                var configManager = new ConfigurationManager<OpenIdConnectConfiguration>(
-                    metadataUri.AbsoluteUri,
-                    new OpenIdConnectConfigurationRetriever(),
-                    new HttpDocumentRetriever(httpClient) { RequireHttps = true }
-                );
-
-                var config = await configManager.GetConfigurationAsync(new CancellationToken());
-
-                var tokenValidationParameters = new TokenValidationParameters
+                if (this.tokenValidationParameters == null)
                 {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = config.SigningKeys.FirstOrDefault(),
+                    //Grab the metadata
+                    using (var handler = new HttpClientHandler())
+                    using (var httpClient = new HttpClient(handler))
+                    {
+                        httpClient.Timeout = TimeSpan.FromMinutes(1);
+                        httpClient.MaxResponseContentBufferSize = 1024 * 1024 * 10; // 10 MB
 
-                    ValidateIssuer = true,
-                    ValidIssuer = config.Issuer,
+                        Uri metadataUri = new Uri(new Uri(options.Authority), options.MetadataPath);
 
-                    ValidateAudience = false,
+                        var configManager = new ConfigurationManager<OpenIdConnectConfiguration>(
+                            metadataUri.AbsoluteUri,
+                            new OpenIdConnectConfigurationRetriever(),
+                            new HttpDocumentRetriever(httpClient) { RequireHttps = true }
+                        );
 
-                    ValidateLifetime = false,
-                    ClockSkew = TimeSpan.FromSeconds(5),
-                };
+                        var config = await configManager.GetConfigurationAsync(new CancellationToken());
 
-                return tokenValidationParameters;
+                        this.tokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuerSigningKey = true,
+                            IssuerSigningKey = config.SigningKeys.FirstOrDefault(),
+
+                            ValidateIssuer = true,
+                            ValidIssuer = config.Issuer,
+
+                            ValidateAudience = false,
+
+                            ValidateLifetime = false,
+                            ClockSkew = TimeSpan.FromSeconds(5),
+                        };
+                    }
+                }
             }
+            finally
+            {
+                semaphoreSlim.Release();
+            }
+
+            return this.tokenValidationParameters;
         }
     }
 }
