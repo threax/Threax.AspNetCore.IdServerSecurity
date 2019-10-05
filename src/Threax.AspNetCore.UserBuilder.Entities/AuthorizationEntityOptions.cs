63369b0edec52cf594c2b09bc5a7db6dc33f421b
Andrew Piper <andrew@threax.com>
2019-10-05 13:16:30 -0400

    Compiled against net core 3.0 for remaining libraries that needed it.

--- src/Threax.AspNetCore.UserBuilder.Entities/AuthorizationEntityOptions.cs
+++ src/Threax.AspNetCore.UserBuilder.Entities/AuthorizationEntityOptions.cs
@@ -1,32 +1,69 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Infrastructure;
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Threax.AspNetCore.UserBuilder.Entities
 {
     public class AuthorizationEntityOptions : IDbContextOptionsExtension
-    {
+    { 
         public String UserTableName { get; set; } = "spc.auth.Users";
 
         public String RoleTableName { get; set; } = "spc.auth.Roles";
 
         public String UserToRoleTableName { get; set; } = "spc.auth.UsersToRoles";
 
-        public string LogFragment => "";
+#if NETCOREAPP3_0
+
+        private ContextOptionsExtensionsInfo extensionInfo;
+
+        public AuthorizationEntityOptions()
+        {
+            extensionInfo = new ContextOptionsExtensionsInfo(this);
+        }
+
+        public DbContextOptionsExtensionInfo Info => extensionInfo;
+
+        public void ApplyServices(IServiceCollection services)
+        {
+            
+        }
+        public class ContextOptionsExtensionsInfo : DbContextOptionsExtensionInfo
+        {
+            public ContextOptionsExtensionsInfo(IDbContextOptionsExtension extension) : base(extension)
+            {
+            }
+
+            public override bool IsDatabaseProvider => true;
+
+            public override string LogFragment => "";
+
+            public override long GetServiceProviderHashCode()
+            {
+                return 0;
+            }
+
+            public override void PopulateDebugInfo([NotNullAttribute] IDictionary<string, string> debugInfo)
+            {
+
+            }
+        }
+
+#elif NETSTANDARD2_0
 
         public bool ApplyServices(IServiceCollection services)
         {
             return false;
         }
 
+        public string LogFragment => "";
+
         public long GetServiceProviderHashCode()
         {
             return 0;
         }
+#endif
 
         public void Validate(IDbContextOptions options)
         {
