45b61cbb4b46d4d090fabfc0b91b2a2f87664616
Andrew Piper <andrew@threax.com>
2021-01-19 15:33:17 -0500

    Added push script.

---
+++ Push.ps1
@@ -0,0 +1,8 @@
+git push
+git push --tags
+git push https://github.com/threax/Threax.AspNetCore.IdServerSecurity.git
+git push https://github.com/threax/Threax.AspNetCore.IdServerSecurity.git --tags
+git push https://gitlab.com/threax-software/threax.aspnetcore.idserversecurity.git
+git push https://gitlab.com/threax-software/threax.aspnetcore.idserversecurity.git --tags
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.AspNetCore.Template
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.AspNetCore.Template --tags
