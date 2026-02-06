eb69164449486a83d162b673b62438add40e176d
Andrew Piper <andrew@threax.com>
2026-02-06 11:51:29 -0500

    Replace IdentityModel with Duende.IdentityModel.

--- src/Threax.AspNetCore.JwtCookieAuth/JwtCookieAuthenticationHandler.cs
+++ src/Threax.AspNetCore.JwtCookieAuth/JwtCookieAuthenticationHandler.cs
@@ -1,4 +1,4 @@
-﻿using IdentityModel.Client;
+﻿using Duende.IdentityModel.Client;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Http;
